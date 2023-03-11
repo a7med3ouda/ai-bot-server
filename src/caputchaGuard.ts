65ed19c3f07821eca6886ac07795c1fbead3b480
a7med3ouda <oudacode@gmail.com>
2023-03-11 20:53:48 +0200

    caputcha guard

---
+++ src/caputchaGuard.ts
@@ -0,0 +1,26 @@
+import { verify } from "hcaptcha";
+import { Request, Response, NextFunction } from "express";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers["X-CAPUTCHA-TOKEN"] as string;
+
+    if (!token)
+      return res
+        .status(400)
+        .json({ subject: "caputcha", message: "Invalid captcha" });
+
+    const data = await verify(process.env.CAPUTCHA_SECRET, token);
+    if (data.success === true) {
+      next();
+    } else {
+      return res
+        .status(400)
+        .json({ subject: "caputcha", message: "Invalid captcha" });
+    }
+  } catch (error: any) {
+    return res
+      .status(400)
+      .json({ subject: "caputcha", message: error.message });
+  }
+};
